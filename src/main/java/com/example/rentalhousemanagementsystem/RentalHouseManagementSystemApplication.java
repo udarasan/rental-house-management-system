13f544cc588addc7052f67b649a3e5627d797e7a
udarasan <udarassanjeewa@gmail.com>
2022-03-18 11:40:53 +0530

    Initial commit

---
+++ src/main/java/com/example/rentalhousemanagementsystem/RentalHouseManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentalhousemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalHouseManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalHouseManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rentalhousemanagementsystem/RentalHouseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentalhousemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalHouseManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
