0b9ad8d708615d04b8ff893d927e06b47e0cc910
udarasan <udarassanjeewa@gmail.com>
2022-05-16 20:39:19 +0530

    cros error

--- src/main/java/com/example/rentalhousemanagementsystem/RentalHouseManagementSystemApplication.java
+++ src/main/java/com/example/rentalhousemanagementsystem/RentalHouseManagementSystemApplication.java
@@ -4,8 +4,18 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class RentalHouseManagementSystemApplication {
@@ -21,5 +31,6 @@ public class RentalHouseManagementSystemApplication {
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
 
 }
