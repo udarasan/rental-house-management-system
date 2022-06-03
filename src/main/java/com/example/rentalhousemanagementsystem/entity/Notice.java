b44be1b1ad67827573a7929033270b5119af9c5e
udarasan <udarassanjeewa@gmail.com>
2022-06-03 10:19:10 +0530

    GenerationType.IDENTITY

--- src/main/java/com/example/rentalhousemanagementsystem/entity/Notice.java
+++ src/main/java/com/example/rentalhousemanagementsystem/entity/Notice.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @AllArgsConstructor
@@ -15,6 +13,7 @@ import javax.persistence.Table;
 @Table(name = "notice")
 public class Notice {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String subject;
     private String message;
